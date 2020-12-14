df59b4e054ecc6724135d737416dea2b548087af
陈亮陈亮 <chenliang@hd123.com>
2020-12-14 15:03:03 +0800

    update querycondition

--- template/src/components/query-condition/QueryCondition.ts
+++ template/src/components/query-condition/QueryCondition.ts
@@ -5,9 +5,14 @@ import { Vue, Prop, Component } from 'vue-property-decorator'
 })
 export default class QueryCondition extends Vue {
   opened: boolean = false // 是否展开
+  @Prop({ type: Boolean, default: false }) submitLoading: boolean // 提交按钮的loading状态
+  @Prop({ type: Number, default: 1 }) rowNumber: number // 总共有几行
+  @Prop({ type: Number, default: 1 }) searchLocationRow: number // 查询和重置操作在第几行
   @Prop({ type: Boolean, default: false }) toggle: boolean // 允许折叠
   @Prop({ type: Boolean, default: true }) showButton: boolean // 是否显示查询重置按钮
   $refs: any
+  conditionOperate: string = 'condition-operation'
+
   /**
    * 键盘事件
    */
@@ -31,5 +36,17 @@ export default class QueryCondition extends Vue {
 
   doToggle() {
     this.opened = !this.opened
+    this.conditionOperate = this.opened ? 'condition-operation active' : 'condition-operation'
+  }
+  showRow(row: number) {
+    if (this.toggle === false) {
+      return true
+    } else {
+      if (this.opened) {
+        return true
+      } else {
+        return row === this.searchLocationRow
+      }
+    }
   }
 }
--- template/src/components/query-condition/QueryCondition.vue
+++ template/src/components/query-condition/QueryCondition.vue
@@ -1,18 +1,20 @@
 <template>
   <el-form label-width="96px" ref="queryCondition" class="query-condition" @keydown.native="bindFastCode">
-    <el-row class="opened-action-row" :gutter="20">
-      <slot></slot>
-      <template v-if="opened && $slots.closed">
-        <slot name="closed"></slot>
-      </template>
+    <div v-for="(item, index) in rowNumber" :key="index">
+      <el-row class="opened-action-row" :gutter="20" v-if="showRow(index + 1)">
+        <slot :name="'row-' + (index + 1)"></slot>
+        <template v-if="opened && $slots.closed && searchLocationRow === index + 1">
+          <slot name="closed"></slot>
+        </template>
 
-      <el-col :span="8" v-if="showButton" class="condition-operation">
-        <el-button size="medium" type="primary" @click="doSearch">查询</el-button>
-        <el-button size="medium" @click="doReset">重置</el-button>
-        <el-button size="medium" type="text" v-if="toggle && opened" @click="doToggle" icon="el-icon-arrow-up">收起</el-button>
-        <el-button size="medium" type="text" v-if="toggle && !opened" @click="doToggle" icon="el-icon-arrow-down">展开</el-button>
-      </el-col>
-    </el-row>
+        <el-col :span="8" v-if="showButton && searchLocationRow === index + 1" :class="conditionOperate">
+          <el-button size="medium" type="primary" @click="doSearch" :loading="submitLoading">查询</el-button>
+          <el-button size="medium" @click="doReset">重置</el-button>
+          <el-button size="medium" type="text" v-if="toggle && opened" @click="doToggle" icon="el-icon-arrow-up">收起</el-button>
+          <el-button size="medium" type="text" v-if="toggle && !opened" @click="doToggle" icon="el-icon-arrow-down">展开</el-button>
+        </el-col>
+      </el-row>
+    </div>
   </el-form>
 </template>
 
@@ -26,6 +28,7 @@
     display: flex;
     justify-content: flex-start;
     flex-wrap: wrap;
+    margin-top: 12px;
     .condition-operation {
       box-sizing: border-box;
       padding-left: 96px;
@@ -37,6 +40,10 @@
 
   .el-select {
     width: 100%;
+  }
+
+  .condition-operation {
+    padding-top: 2px;
   }
 }
 </style>
